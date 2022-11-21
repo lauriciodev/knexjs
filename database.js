efe4f1d225654799fef8bcd5485ea8d4d1caec28
LauricioX <developerwebmasterpro@gmail.com>
2022-11-21 17:07:45 -0300

    wss

---
+++ database.js
@@ -0,0 +1,10 @@
+const knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "berlim2062",
+    database: "knex",
+  },
+});
